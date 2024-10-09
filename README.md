b6972164b4f9daf4ac976a7832a9e471bb2bbc57
codii-x <antonynivin074@gmail.com>
2024-10-09 10:55:02 +0000

    condition

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CodeCrafters2.0
+Nivin Antony
---
+++ main.py
@@ -0,0 +1,9 @@
+number_1: int = int(intput("enter a number 1: "))
+number_2: int = int(intput("enter a number 2: "))
+
+if number 1 == number 2:
+    print("both are same")
+elif number_1 > number_2:
+    print(number_1"is greater")
+elif number_1 < number_2:
+    print(number_2,"is greater")  
